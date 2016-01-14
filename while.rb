7b80ff06db847cbf93f051e8e0569c6ecff40452
Sho Kobayashi <a5828782@icloud.com>
2016-01-14 16:02:27 +0900

    whileと.timesメソッドで繰り返しの練習

---
+++ while.rb
@@ -0,0 +1,11 @@
+i = 1
+while i <= 10 do
+    print(i, "\n")
+    i = i + 1
+end
+
+#timesメソッドで回数制限をかける
+#All work and tno play makes Jack a dull boy.と10行表示
+10.times {
+    print("All work and tno play makes Jack a dull boy.\n")
+}
