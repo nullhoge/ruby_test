1914c091ab1625bc918799975e7a956bc9646872
Sho Kobayashi <a5828782@icloud.com>
2016-01-14 15:53:57 +0900

    if-then-end文による条件判断の練習

---
+++ bigger_smaller.rb
@@ -0,0 +1,15 @@
+a = 20
+if a >= 10 then
+    print("bigger\n")
+end
+if a <= 9 then
+    print("smaller\n")
+end
+
+#thenは省略可
+if a >= 10
+    print("bigger\n")
+#elseで条件一致しなかった時の処理も可
+else
+    print("smaller\n")
+end
